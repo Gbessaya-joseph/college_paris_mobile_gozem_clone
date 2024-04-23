d6de29e031d0aa095df255c5030956a9ef11045b
maryes228 <carinehonou@gmail.com>
2024-04-23 10:02:38 +0000

    Setup app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:gozem_clone/features/home/screens/home_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: "Gozem Clone",
+      home: HomeScreen(),
     );
   }
 }
